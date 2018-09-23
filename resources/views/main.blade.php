f5ce9ee9e36de8bd39577de005e6d100b7b8e6f1
Isidro Vasquez <isidroevc@gmail.com>
2018-09-23 14:20:07 -0500

    muertisimo

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,55 +4,85 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{url('/css/main/main.css')}}">
+   <!-- <link rel="stylesheet" href="{{url('/css/materialize.css')}}">-->
     <title>Colorcentro</title>
 </head>
 
 <body>
     <div class="contenedor">
-        <header>
-            <div class="logo">
-                <img src="images/logo.jpg" width="150" alt="">
-                <a href="#">Colorcentro</a>
-            </div>
-
-            <nav>
-                <div class="nav-enlace">
-                    <a href="#">INICIO</a>
-                </div>
-                <div class="nav-enlace">
-                    <a href="#">NOSOTROS</a>
-                </div>
-                <div class="nav-enlace">
-                    <a href="#">NUESTROS<BR> PRODUCTOS</a>
-                </div>
-                <div class="nav-enlace">
-                    <a href="#">SUCURSALES</a>
-                </div>
-                <div class="nav-<a>enlace">
-                    <a href="#">APPS</a>
-                </div>
-                <div class="nav-enlace">
-                    <a href="#">CONTACTO</a>
-                </div>
-            </nav>
-        </header>
+        
         <div class="sec-1">
-
+            <div class="header">
+                <div class="logo">
+                    <a href="#">
+                    <img src="{{url('/img/colorcentro_logo_sc.png')}}" width="150" alt=""></a>
+                </div>
+    
+                <nav>
+                    <div class="nav-enlace">
+                        <a href="#">INICIO</a>
+                    </div>
+                    <div class="nav-enlace">
+                        <a href="#">NOSOTROS</a>
+                    </div>
+                    <div class="nav-enlace">
+                        <a href="#">NUESTROS<BR> PRODUCTOS</a>
+                    </div>
+                    <div class="nav-enlace">
+                        <a href="#">SUCURSALES</a>
+                    </div>
+                    <div class="nav-enlace">
+                        <a href="#">APPS</a>
+                    </div>
+                    <div class="nav-enlace">
+                        <a href="#">CONTACTO</a>
+                    </div>
+                </nav>
+            </div>
 
         </div>
         <div class="sec-2">
             <div class="lema-contenedor">
-
+                <div class="lema-texto">
+                    <p class = "lema">
+                        Siempre hemos pensado que la satisfacción del cliente es la escencia de la calidad.
+                    </p>
+                </div>
             </div>
             <div class="nosotros">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </div>
+                <p class="nosotros">NOSOTROS</p>
+                <p class="leyenda">Nuestra empresa tuvo inicio gracias a la inspiración de una tienda familiar la cual fue creada 
+                   muchos años atrás en la ciudad de <b>Cd. Obregón, Son.</b></p>
+
+                <p class="leyenda">Siendo nuestro único fin el de independizarnos y mostrarle a  la gente una experiencia de compra 
+                diferente, dándole la mejor elección en recubrimientos, para <b>crear espacios modernos y con armonía.</b></p>
+
+                <p class="leyenda">
+                Por ésa razón elegimos la <b>marca Sherwin Williams</b> ya que cuenta con más de <b>150 años en el 
+                mercado</b> siendo líder de fabricación de pinturas para el hogar y la industria, contando con una 
+                amplia gama de productos y aplicaciones que harán más sencilla la elección de color para tu hogar 
+                o empresa.
+                </p>
+
+                <p class="titulo-sec-2">Misión</p>
+
+                <p class="leyenda">
+                    Proporcionar a <b>nuestros clientes</b> productos y servicios de la más alta calidad, creando así una 
+                    <b> experiencia de compra única</b> y diferente en cuanto a <b>recubrimiento se refiere</b>.
+                </p>
+
+                <p class="titulo-sec-2">Visión</p>
+
+                <p class="leyenda">
+                    Ser el <b> distribuidor de Sherwin Williams</b> que ofrezca la mejor atención a sus clientes a través de 
+                    todas nuestras herramientas y aplicaciones generando así clientes satisfechos, cumpliendo los 
+                    estatutos marcados por la empresa sirviendo con <b> honestidad y compromiso</b> de la <b>familia Color Centro</b>
+                </p>
+                
         </div>
-        <div class="sec-3">
-            nadie
-        </div>
+        
     </div>
+    <!--<script> src=href="{{url('/js/materialize.js')}}"</script>-->
 </body>
 
 </html>
