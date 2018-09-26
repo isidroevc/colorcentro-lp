28d3ea7806f04b3c390587a4036ecb3151eba3ab
Isidro Vasquez <isidroevc@gmail.com>
2018-09-26 09:01:52 -0500

    nada

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,20 +4,20 @@
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="{{url('/css/main/main.css')}}">
-   <!-- <link rel="stylesheet" href="{{url('/css/materialize.css')}}">-->
+    <!-- <link rel="stylesheet" href="{{url('/css/materialize.css')}}">-->
     <title>Colorcentro</title>
 </head>
 
 <body>
     <div class="contenedor">
-        
+
         <div class="sec-1">
             <div class="header">
                 <div class="logo">
                     <a href="#">
-                    <img src="{{url('/img/colorcentro_logo_sc.png')}}" width="150" alt=""></a>
+                        <img src="{{url('/img/colorcentro_logo_sc.png')}}" width="150" alt=""></a>
                 </div>
-    
+
                 <nav>
                     <div class="nav-enlace">
                         <a href="#">INICIO</a>
@@ -44,48 +44,184 @@
         <div class="sec-2">
             <div class="lema-contenedor">
                 <div class="lema-texto">
-                    <p class = "lema">
+                    <p class="lema">
                         Siempre hemos pensado que la satisfacción del cliente es la escencia de la calidad.
                     </p>
                 </div>
             </div>
             <div class="nosotros">
                 <p class="nosotros">NOSOTROS</p>
-                <p class="leyenda">Nuestra empresa tuvo inicio gracias a la inspiración de una tienda familiar la cual fue creada 
-                   muchos años atrás en la ciudad de <b>Cd. Obregón, Son.</b></p>
+                <p class="leyenda">Nuestra empresa tuvo inicio gracias a la inspiración de una tienda familiar la cual
+                    fue creada
+                    muchos años atrás en la ciudad de <b>Cd. Obregón, Son.</b></p>
 
-                <p class="leyenda">Siendo nuestro único fin el de independizarnos y mostrarle a  la gente una experiencia de compra 
-                diferente, dándole la mejor elección en recubrimientos, para <b>crear espacios modernos y con armonía.</b></p>
+                <p class="leyenda">Siendo nuestro único fin el de independizarnos y mostrarle a la gente una
+                    experiencia de compra
+                    diferente, dándole la mejor elección en recubrimientos, para <b>crear espacios modernos y con
+                        armonía.</b></p>
 
                 <p class="leyenda">
-                Por ésa razón elegimos la <b>marca Sherwin Williams</b> ya que cuenta con más de <b>150 años en el 
-                mercado</b> siendo líder de fabricación de pinturas para el hogar y la industria, contando con una 
-                amplia gama de productos y aplicaciones que harán más sencilla la elección de color para tu hogar 
-                o empresa.
+                    Por ésa razón elegimos la <b>marca Sherwin Williams</b> ya que cuenta con más de <b>150 años en el
+                        mercado</b> siendo líder de fabricación de pinturas para el hogar y la industria, contando con
+                    una
+                    amplia gama de productos y aplicaciones que harán más sencilla la elección de color para tu hogar
+                    o empresa.
                 </p>
 
                 <p class="titulo-sec-2">Misión</p>
 
                 <p class="leyenda">
-                    Proporcionar a <b>nuestros clientes</b> productos y servicios de la más alta calidad, creando así una 
+                    Proporcionar a <b>nuestros clientes</b> productos y servicios de la más alta calidad, creando así
+                    una
                     <b> experiencia de compra única</b> y diferente en cuanto a <b>recubrimiento se refiere</b>.
                 </p>
 
                 <p class="titulo-sec-2">Visión</p>
 
                 <p class="leyenda">
-                    Ser el <b> distribuidor de Sherwin Williams</b> que ofrezca la mejor atención a sus clientes a través de 
-                    todas nuestras herramientas y aplicaciones generando así clientes satisfechos, cumpliendo los 
-                    estatutos marcados por la empresa sirviendo con <b> honestidad y compromiso</b> de la <b>familia Color Centro</b>
+                    Ser el <b> distribuidor de Sherwin Williams</b> que ofrezca la mejor atención a sus clientes a
+                    través de
+                    todas nuestras herramientas y aplicaciones generando así clientes satisfechos, cumpliendo los
+                    estatutos marcados por la empresa sirviendo con <b> honestidad y compromiso</b> de la <b>familia
+                        Color Centro</b>
                 </p>
-                
+
+            </div>
+
         </div>
-        
-    </div>
-    <div class = "sec-3">
-        <p class="nuestros-productos">NUESTROS PRODUCTOS</p>
-    </div>
-    <!--<script> src=href="{{url('/js/materialize.js')}}"</script>-->
+        <div class="sec-3">
+            <p class="nuestros-productos">NUESTROS PRODUCTOS</p>
+
+            <div class="row-producto">
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/pintura/primarios.html">
+                        <div>
+                            <img src="{{url('/img/primarios.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">PRIMARIOS</p>
+                    </a>
+                </div>
+
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/pintura/vinilicas.html">
+                        <div>
+                            <img src="{{url('/img/vinilicas.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">VINILICAS</p>
+                    </a>
+                </div>
+
+
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/pintura/esmaltes.html">
+                        <div>
+                            <img src="{{url('/img/esmaltes.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">ESMALTES</p>
+                    </a>
+                </div>
+
+            </div>
+            <div class="row-producto">
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/pintura/industrial-ligero.html">
+                        <div>
+                            <img src="{{url('/img/anti-graffiti.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">INDUSTRIAL LIGERO</p>
+                    </a>
+
+                </div>
+
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/accesorios-y-equipo/brochas.html">
+                        <div>
+                            <img src="{{url('/img/brochas_rodillos.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">BROCHAS Y RODILLOS</p>
+                    </a>
+                </div>
+
+
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/accesorios-y-equipo/aerosoles.html">
+                        <div>
+                            <img src="{{url('/img/aerosoles.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">AEOROSOLES</p>
+                    </a>
+                </div>
+
+            </div>
+            <div class="row-producto">
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/accesorios-y-equipo/graco.html">
+                        <div>
+                            <img src="{{url('/img/pistolas.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">PISTOLAS PARA PINTAR (GRACO)</p>
+                    </a>
+                </div>
+
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/accesorios-y-equipo/accesorios-en-general.html">
+                        <div>
+                            <img src="{{url('/img/accesorios_gral.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">ACCESORIOS EN GRAL.</p>
+                    </a>
+                </div>
+
+
+                <div class="contenedor-producto">
+                    <a href="http://www.sherwinmexico.com/es/barnices-y-tintes.html">
+                        <div>
+                            <img src="{{url('/img/maderas.PNG')}}" class="contenedor-imagen-producto">
+                        </div>
+                        <p class="titulo-producto">MADERAS</p>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+        <div class="sec-4">
+        </div>
+
+        <div class="sec-5">
+            <div class="row-sec-5">
+                <div class="parte">
+                    <img class="logo-snap-contenedor" src="{{url('/img/color_snap_1.png')}}" alt="">
+                </div>
+                <div class="parte">
+                    <div>
+                        <img class="logo-snap-contenedor" src="{{url('/img/colorsnaplogo.png')}}" alt="">
+                    </div>
+                    <div class="boton-entrar-contenedor">
+                        <p class="entrar">Entrar</p>
+                    </div>
+
+                </div>
+                <div class="parte">
+                    <img class="logo-snap-contenedor" src="{{url('/img/color_snap_2.png')}}" alt="">
+                </div>
+            </div>
+
+
+
+        </div>
+        <div class="sec-6">
+            <div class="obtener-navegador">
+                <div class="div-contenedor">
+                    <img src="{{url('/img/sherwin-header.png')}}" alt="">
+                </div>
+                <div class="div-contenedor">
+                    <p class="obtener-navegador">
+                        Obtener Color Snap Para Tu Navegador
+                    </p>
+                </div>
+            </div>
+        </div>
+        <!--<script> src=href="{{url('/js/materialize.js')}}"</script>-->
 </body>
 
 </html>
