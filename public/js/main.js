a2640ccfe34687a2da5c99f1c9f34e44b20f71fe
Daniel Estrada <isc.daniel.estrada@gmail.com>
2018-09-27 19:34:00 -0500

    limipar formulario

--- public/js/main.js
+++ public/js/main.js
@@ -11,16 +11,15 @@ $("#form-contacto").on('submit', function(evt) {
         compania: compania,
         mensaje: mensaje
     };
-    console.log(data);
     $.ajax({
         type: "POST",
         url: './api/mensaje',
         data: data,
         success: function(data, status) {
-            showModal("Se ha enviado el formulario.");
+            showModal("Se ha enviado tu mensaje.");
         },
         error: function(err) {
-            showModal("Error al enviar su mensaje, intente más tarde.");
+            showModal("Hubo un error enviando tu mensaje, inténtalo más tarde.");
         }
     });
 });
@@ -34,18 +33,24 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+    clearForm();
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+        clearForm();
     }
 }
 
 let showModal = message => {
     let text = document.getElementById("modal-message");
 
-    text.innerHTML = message
+    text.innerHTML = message;
     modal.style.display = "block";
+}
+
+let clearForm = () => {
+    document.getElementById("form-contacto").reset();
 }
